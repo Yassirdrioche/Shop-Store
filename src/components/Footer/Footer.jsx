a85ff89706cdfdae4459f5565e2f15c09b8ca49c
Yassirdrioche <yassirdroiche@gmail.com>
2025-03-13 17:30:08 +0000

    FIXED::CLASS ERROR

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,14 +6,14 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="bg-gradient-to-r from-neutral-900 to-neutral-800 text-white py-12 relative overflow-hidden">
-      <div class="custom-shape-divider-top-1741878716">
+      <div className="custom-shape-divider-top-1741878716">
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1200 120"
           preserveAspectRatio="none"
         >
-          <path d="M1200 0L0 0 892.25 114.72 1200 0z" class="shape-fill"></path>
+          <path d="M1200 0L0 0 892.25 114.72 1200 0z" className="shape-fill"></path>
         </svg>
       </div>
       {/* Wave Effect */}
