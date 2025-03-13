0c37c3fe5ec6c6f5c25f0ebfb47a1e6c01f90d0f
Yassirdrioche <yassirdroiche@gmail.com>
2025-03-13 02:03:49 +0000

    CHANGED::CURSOR HIDDEN IN MOBILE VIEWS

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -76,8 +76,8 @@ const Header = () => {
         <header
           className={`fixed md:w-[70%] w-[90%] z-[9999] top-3 rounded-l-full rounded-r-full transition-all duration-300  px-4 ${
             isScrolled
-              ? "shadow-lg bg-black bg-opacity-30 text-white backdrop-blur-lg"
-              : "bg-neutral-100    mix-blend-difference"
+              ? "shadow-lg bg-black bg-opacity-20 text-white backdrop-blur-lg"
+              : "bg-neutral-100 "
           }
           ${
             isToggled || isFilterSideBarOpen
