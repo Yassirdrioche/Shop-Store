edf44e98b4cd274a07160b071090ecf441ca182a
Yassirdrioche <yassirdroiche@gmail.com>
2025-05-14 11:44:15 +0100

    Update Header width in CSS

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -87,7 +87,7 @@ const Header = () => {
     <>
       <section className="container mx-auto flex justify-center">
         <header
-          className={`fixed md:w-[70%] w-[90%] z-[9999] top-3 rounded-l-full rounded-r-full transition-all duration-300  px-4 ${
+          className={`fixed md:w-[80%]  w-[90%] z-[9999] top-3 rounded-l-full rounded-r-full transition-all duration-300  px-4 ${
             isScrolled
               ? "shadow-lg bg-black bg-opacity-20 text-white backdrop-blur-lg"
               : "bg-neutral-200 "
